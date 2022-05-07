9e4cfa5800e07e12d3594e8b16e0b65190aec3fa
ihwani <33796218+ihwani@users.noreply.github.com>
2022-05-07 15:10:12 +0900

    .

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -3,9 +3,6 @@ import 'package:url_launcher/url_launcher.dart';
 List foundList = [];
 
 Future<void> launchURL(String url) async {
-  await launch(
-    url,
-    forceSafariVC: true,
-    forceWebView: true,
-  );
+  await launch(url,
+      forceSafariVC: true, forceWebView: true, enableJavaScript: true);
 }
